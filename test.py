4bb685d59a55244712df8700eacb0546132e1926
feiyu <1422675202@qq.com>
2023-12-11 00:08:58 +0800

    modify: show content of csv

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import pandas as pd
 
 # 设置上传文件的保存目录
 UPLOAD_DIRECTORY = "./uploaded_files"
@@ -14,8 +15,8 @@ def save_uploaded_file(uploaded_file):
         file_path = os.path.join(UPLOAD_DIRECTORY, uploaded_file.name)
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
-        return True
-    return False
+        return file_path  # 返回文件的保存路径
+    return None
 
 def list_files(directory):
     """列出目录中的所有文件并返回它们"""
@@ -27,13 +28,20 @@ def list_files(directory):
     return files
 
 def main():
-    st.title("文件上传示例")
+    st.title("文件上传")
 
-    uploaded_file = st.file_uploader("选择一个文件", type=None)
+    uploaded_file = st.file_uploader("选择一个文件", type=None)  # 允许任何类型的文件
     if uploaded_file is not None:
         # 文件保存到服务器
-        if save_uploaded_file(uploaded_file):
+        file_path = save_uploaded_file(uploaded_file)
+        if file_path:
             st.success(f"文件 {uploaded_file.name} 已成功上传到服务器！")
+            # 如果文件是 CSV 文件，读取并显示
+            if uploaded_file.name.endswith('.csv'):
+                df = pd.read_csv(file_path)
+                st.write(df)
+            else:
+                st.info("已上传非 CSV 文件，无法显示内容。")
         else:
             st.error("文件上传失败。")
 
