4b91f49a106b88a627413bc895460d7710959d64
feiyu <1422675202@qq.com>
2023-12-11 16:14:43 +0800

    modify: hide icon

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+[server]
+headless = true
+enableCORS = false
+enableXsrfProtection = false
+port = $PORT
+[client]
+showErrorDetails = true
+[browser]
+gatherUsageStats = false
+[theme]
+base='dark'
+primaryColor='#f63366'
+backgroundColor='#0e1117'
+secondaryBackgroundColor='#262730'
+textColor='#ffffff'
+font='sans serif'
+[deprecation]
+showMenu = false
 " > ~/.streamlit/config.toml
