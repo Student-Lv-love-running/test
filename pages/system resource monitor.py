df1038388ebfe467bb59131beb73e915b15777d8
feiyu <1422675202@qq.com>
2023-12-13 10:51:53 +0800

    modify2.1: font style

--- pages/system resource monitor.py
+++ pages/system resource monitor.py
@@ -4,7 +4,7 @@ import pandas as pd
 import psutil
 import matplotlib.pyplot as plt
 
-plt.rcParams['font.sans-serif'] = ['SimHei']  # 设置中文字体为黑体
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 设置中文字体为黑体
 plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
 
 def system_monitor_page():
