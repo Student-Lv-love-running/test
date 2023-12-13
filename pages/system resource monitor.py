11cb9b903b814807a9389b14109710e4232b8cae
feiyu <1422675202@qq.com>
2023-12-13 11:05:35 +0800

    modify2.1: font style

--- pages/system resource monitor.py
+++ pages/system resource monitor.py
@@ -5,7 +5,7 @@ import psutil
 import matplotlib.pyplot as plt
 
 
-plt.rcParams['font.sans-serif'] = ['DejaVu Sans']  # 设置中文字体为黑体
+# plt.rcParams['font.sans-serif'] = ['DejaVu Sans']  # 设置中文字体为黑体
 plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
 
 def system_monitor_page():
